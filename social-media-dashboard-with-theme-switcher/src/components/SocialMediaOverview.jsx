2d824cc364815b3a5d88398ad135b8072aeea5e5
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-02-15 14:05:57 +0200

    Added transitioning effects

--- social-media-dashboard-with-theme-switcher/src/components/SocialMediaOverview.jsx
+++ social-media-dashboard-with-theme-switcher/src/components/SocialMediaOverview.jsx
@@ -8,14 +8,14 @@ function SocialMediaOverview ({ isDarkMode }) {
             {data.map((social) => {
                 return (
                     <div
-                        className={`rounded-md flex flex-col items-center gap-4 text-center py-4 ${isDarkMode ? "bg-[hsl(228,28%,20%)]" : "bg-[hsl(227,47%,96%)]"}`}
+                        className={`rounded-md flex flex-col items-center gap-4 text-center py-4 transition-all duration-300 ease-in-out ${isDarkMode ? "bg-[hsl(228,28%,20%)]" : "bg-[hsl(227,47%,96%)]"}`}
                         key={social.social}>
                         <div className="flex gap-4">
                             <img src={social.socialIcon} alt={`${social.social} icon`} />
-                            <span className={`font-bold ${isDarkMode ? "text-[hsl(230,22%,74%)]" : "text-[hsl(228,12%,44%)]"}`}>{social.userSocialHandle}</span>
+                            <span className={`font-bold transition-all duration-300 ease-in-out ${isDarkMode ? "text-[hsl(230,22%,74%)]" : "text-[hsl(228,12%,44%)]"}`}>{social.userSocialHandle}</span>
                         </div>
                         <div className="flex flex-col">
-                            <span className={`text-5xl font-bold ${isDarkMode ? "text-[hsl(0,100%,100%)]" : ""}`}>{social.noOfFollowers}</span>
+                            <span className={`text-5xl font-bold transition-all duration-300 ease-in-out ${isDarkMode ? "text-[hsl(0,100%,100%)]" : ""}`}>{social.noOfFollowers}</span>
                             <span className="uppercase tracking-[0.25rem] text-[hsl(228,12%,44%)]">{social.followerType}</span>
                         </div>
                         <div className="flex items-center gap-2 font-semibold">
--- social-media-dashboard-with-theme-switcher/src/components/SocialMediaOverviewToday.jsx
+++ social-media-dashboard-with-theme-switcher/src/components/SocialMediaOverviewToday.jsx
@@ -3,7 +3,7 @@ import data from "../data/data"
 function SocialMediaOverviewToday ({ isDarkMode }) {
     return (
         <section>
-            <h2 className={`font-bold text-2xl ${isDarkMode ? "text-[hsl(0,100%,100%)]" : "text-[hsl(228,12%,44%)]"}`}>
+            <h2 className={`font-bold text-2xl transition-all duration-300 ease-in-out ${isDarkMode ? "text-[hsl(0,100%,100%)]" : "text-[hsl(228,12%,44%)]"}`}>
                 Overview - Today
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -13,26 +13,26 @@ function SocialMediaOverviewToday ({ isDarkMode }) {
                             className={`grid grid-cols-1 md:grid-cols-2 gap-4`} 
                             key={socialOverview.social}
                         >
-                            <div className={`flex flex-col gap-4 rounded-md p-4 ${isDarkMode ? "bg-[hsl(228,28%,20%)]" : "bg-[hsl(227,47%,96%)]"}`}>
+                            <div className={`flex flex-col gap-4 rounded-md p-4 transition-all duration-300 ease-in-out ${isDarkMode ? "bg-[hsl(228,28%,20%)]" : "bg-[hsl(227,47%,96%)]"}`}>
                                 <div className="flex justify-between items-center">
-                                    <span className={`font-bold ${isDarkMode ? "text-[hsl(230,22%,74%)]" : "text-[hsl(228,12%,44%)]"}`}>{socialOverview.viewType}</span>
+                                    <span className={`font-bold transition-all duration-300 ease-in-out ${isDarkMode ? "text-[hsl(230,22%,74%)]" : "text-[hsl(228,12%,44%)]"}`}>{socialOverview.viewType}</span>
                                     <img src={socialOverview.socialIcon} alt={`${socialOverview.social} icon`} />
                                 </div>
                                 <div className="flex justify-between items-center">
-                                    <span className={`text-3xl font-bold ${isDarkMode ? "text-[hsl(0,100%,100%)]" : ""}`}>{socialOverview.noOfViews}</span>
+                                    <span className={`text-3xl font-bold transition-all duration-300 ease-in-out ${isDarkMode ? "text-[hsl(0,100%,100%)]" : ""}`}>{socialOverview.noOfViews}</span>
                                     <div className="flex items-center gap-2">
                                         <img src={socialOverview.upOrDownIcon} alt="Up or down icon" />
                                         <span>{socialOverview.percentageViews}</span>
                                     </div>
                                 </div>
                             </div>
-                            <div className={`flex flex-col gap-4 rounded-md p-4 ${isDarkMode ? "bg-[hsl(228,28%,20%)]" : "bg-[hsl(227,47%,96%)]"}`}>
+                            <div className={`flex flex-col gap-4 rounded-md p-4 transition-all duration-300 ease-in-out ${isDarkMode ? "bg-[hsl(228,28%,20%)]" : "bg-[hsl(227,47%,96%)]"}`}>
                                 <div className="flex justify-between items-center">
-                                    <span className={`font-bold ${isDarkMode ? "text-[hsl(230,22%,74%)]" : "text-[hsl(228,12%,44%)]"}`}>Likes</span>
+                                    <span className={`font-bold transition-all duration-300 ease-in-out ${isDarkMode ? "text-[hsl(230,22%,74%)]" : "text-[hsl(228,12%,44%)]"}`}>Likes</span>
                                     <img src={socialOverview.socialIcon} alt={`${socialOverview.social} icon`} />
                                 </div>
                                 <div className="flex justify-between items-center">
-                                    <span className={`text-3xl font-bold ${isDarkMode ? "text-[hsl(0,100%,100%)]" : ""}`}>{socialOverview.noOfLikes}</span>
+                                    <span className={`text-3xl font-bold transition-all duration-300 ease-in-out ${isDarkMode ? "text-[hsl(0,100%,100%)]" : ""}`}>{socialOverview.noOfLikes}</span>
                                     <div className="flex items-center gap-2">
                                         <img src={socialOverview.upOrDownIcon} alt="Up or down icon" />
                                         <span>{socialOverview.percentageLikes}</span>
