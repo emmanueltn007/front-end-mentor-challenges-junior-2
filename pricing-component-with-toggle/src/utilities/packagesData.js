00db65c98e77924bb780b979644216f390308d00
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-02-28 21:02:19 +0200

    Added a data file to populate the package plans cards

---
+++ pricing-component-with-toggle/src/utilities/packagesData.js
@@ -0,0 +1,31 @@
+import { data } from "react-router";
+
+const packagesPlans = [
+    {
+        id: 01,
+        name: "Basic",
+        annualPrice: 199.99,
+        monthlyPrice: 19.99,
+        storage: "500 GB Storage",
+        users: "2 Users Allowed",
+        data: "Send Up To 3 GB",
+    },
+    {
+        id: 02,
+        name: "Professional",
+        annualPrice: 249.99,
+        monthlyPrice: 24.99,
+        storage: "1 TB Storage",
+        users: "5 Users Allowed", 
+        data: "Send Up To 10 GB" 
+    },
+    {
+        id: 03,
+        name: "Master",
+        annualPrice: 399.99,
+        monthlyPrice: 39.99,
+        storage: "2 TB Storage",
+        users: "10 Users Allowed",
+        data: "Send Up To 20 GB"
+    }
+];
