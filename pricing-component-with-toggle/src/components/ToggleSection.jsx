b308f1e08124dca1115f681546ca7d0504edeaf5
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-03-02 20:29:32 +0200

    Added gradient background to the toggle button

--- pricing-component-with-toggle/src/components/ToggleSection.jsx
+++ pricing-component-with-toggle/src/components/ToggleSection.jsx
@@ -12,11 +12,25 @@ function ToggleSection ({ isAnnually, handleToggleButton }) {
                     onClick={handleToggleButton}
                     aria-label="toggle switch"
                 >
-                    <img 
-                        className="h-16" 
-                        src={isAnnually ? "/images/toggle-on-solid-full.svg" : "/images/toggle-off-solid-full.svg"}
-                        alt="toggle switch" 
-                    />
+                    {isAnnually ? (
+                        <svg className="h-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                            <defs>
+                                <linearGradient id="toggleGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                                    <stop offset="0%" stopColor="hsl(237,73%,79%)" />
+                                    <stop offset="100%" stopColor="hsl(238,63%,64%)" />
+                                </linearGradient>
+                            </defs>
+                            <path d="M224 128C118 128 32 214 32 320C32 426 118 512 224 512L416 512C522 512 608 426 608 320C608 214 522 128 416 128L224 128zM416 224C469 224 512 267 512 320C512 373 469 416 416 416C363 416 320 373 320 320C320 267 363 224 416 224z" fill="url(#toggleGradient)" /></svg>
+                    ) : (
+                        <svg className="h-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                            <defs>
+                                <linearGradient id="toggleGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                                    <stop offset="0%" stopColor="hsl(237,73%,79%)" />
+                                    <stop offset="100%" stopColor="hsl(238,63%,64%)" />
+                                </linearGradient>
+                            </defs>
+                            <path d="M416 192C486.7 192 544 249.3 544 320C544 390.7 486.7 448 416 448L224 448C153.3 448 96 390.7 96 320C96 249.3 153.3 192 224 192L416 192zM608 320C608 214 522 128 416 128L224 128C118 128 32 214 32 320C32 426 118 512 224 512L416 512C522 512 608 426 608 320zM224 400C268.2 400 304 364.2 304 320C304 275.8 268.2 240 224 240C179.8 240 144 275.8 144 320C144 364.2 179.8 400 224 400z" fill="url(#toggleGradient)" /></svg>
+                    )}
                 </button>
                 <span>
                     Monthly
