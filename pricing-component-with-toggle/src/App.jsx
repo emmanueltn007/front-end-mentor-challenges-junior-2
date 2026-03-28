cb53a47ebc85d3c547e2f0e4ce26a42c7e6a3618
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-03-28 19:00:36 +0200

    made adjustments to background images

--- pricing-component-with-toggle/src/App.jsx
+++ pricing-component-with-toggle/src/App.jsx
@@ -10,15 +10,15 @@ function App() {
   };
 
   return (
-      <main className="h-screen w-screen bg-[hsl(240,78%,98%)] font-['Montserrat',sans-serif] flex flex-col justify-center gap-8 max-md:px-8 relative z-100">
-        <div className="h-[30%] w-[40%] md:h-7/10 md:w-1/5 absolute top-0 right-0 z-0">
+      <main className="min-h-screen w-screen bg-[hsl(240,78%,98%)] font-['Montserrat',sans-serif] flex flex-col justify-center gap-8 max-md:px-8 relative z-100">
+        <div className="h-[30%] w-[40%] md:h-[80%] md:w-[20%] absolute top-0 right-0 z-0">
           <img
             className="h-full w-full object-cover"
             src="/images/bg-top.svg"
             alt="top background image"
           />
         </div>
-        <div className="max-md:hidden h-[45%] w-[25%] absolute bottom-0 left-0 z-0">
+        <div className="max-md:hidden h-[40%] w-[20%] absolute bottom-0 left-0 z-0">
           <img
             className="h-full w-full object-cover"
             src="/images/bg-bottom.svg"
