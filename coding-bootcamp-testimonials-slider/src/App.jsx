05f6ae4d5493e1f8f7c795a646a5af443906c005
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-03-31 20:01:41 +0200

    Made further adjustments to the background image

--- coding-bootcamp-testimonials-slider/src/App.jsx
+++ coding-bootcamp-testimonials-slider/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <main className="h-screen w-screen bg-[url('/images/pattern-curve.svg')] bg-contain md:bg-size-[50%] bg-no-repeat bg-bottom-left md:grid md:grid-cols-2 md:p-20 md:place-items-center md:place-content-center relative overflow-hidden">
    
-      <div className="md:right-8 max-md:left-1/2 max-md:top-1/2 max-md:-translate-y-1/2 max-md:-translate-x-1/2 w-[90%] h-full md:w-1/2 md:h-screen absolute -z-5">
+      <div className="md:right-8 max-md:left-1/2 max-md:top-1/2 max-md:-translate-y-1/2 max-md:-translate-x-1/2 w-[75%] h-full md:w-1/2 md:h-screen absolute -z-5">
         <img 
           className="h-fit w-fit" 
           src="/images/pattern-bg.svg" 
