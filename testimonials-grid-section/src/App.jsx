23b128aec8270ef04ca00ca6d2e5bcbe1b861f13
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-04-10 19:52:58 +0200

    Added a font-family and added more styling to the grid

--- testimonials-grid-section/src/App.jsx
+++ testimonials-grid-section/src/App.jsx
@@ -2,7 +2,7 @@ import GridCard from "./components/GridCard";
 
 function App () {
   return (
-    <div className="h-screen md:px-20 md:py-4 bg-[hsl(0,0%,81%)]">
+    <div className="font-['Barlow Semi Condensed',sans-serif] h-screen px-4 py-8 md:px-20 md:py-4 bg-[hsl(0,0%,81%)]">
       <GridCard />
     </div>
   );
--- testimonials-grid-section/src/components/GridCard.jsx
+++ testimonials-grid-section/src/components/GridCard.jsx
@@ -2,30 +2,32 @@ import testimonials from "../utilities/testimonialsData";
 
 function GridCard() {
   return (
-    <div className="h-full grid grid-cols-1 md:grid-cols-4 md:grid-rows-2 md:[grid-auto-rows:minmax(0,1fr)] gap-4">
-      {testimonials.map((testimonial) => {
+    <div className="h-full grid grid-cols-1 md:grid-cols-4 md:grid-rows-2 md:auto-rows-fr gap-4">
+      {testimonials.map((testimonial, index) => {
         return (
           <div 
-            key={testimonial.customer}
+            key={`${testimonial.customer}-${index}`}
             className={testimonial.gridStyle}
           >
             <div className="flex items-center gap-4">
               <img
-                className="h-8 rounded-full border-2 border-[hsl(0,100%,100%)]"
+                className={`h-8 rounded-full border-2 ${testimonial.avatarBorderColor}`}
                 src={testimonial.avatar}
                 alt={`${testimonial.customer}'s avatar`}
               />
               <div className="">
-                <h4 className="font-semibold">{testimonial.customer}</h4>
-                <span className="text-sm">Verified Graduate</span>
+                <h4 className={`font-semibold text-sm ${testimonial.userNameColor}`}>{testimonial.customer}</h4>
+                <span className={`text-xs font-semibold ${testimonial.graduateTextStyle}`}>Verified Graduate</span>
               </div>
             </div>
-            <p className="font-semibold text-lg leading-5">
-              {testimonial.heading}
-            </p>
-            <p className="text-sm leading-5">
-              {testimonial.testimonialParagraph}
-            </p>
+            <div className="flex flex-col gap-4">
+              <p className={`font-semibold text-lg leading-5 ${testimonial.headingStyle}`}>
+                {testimonial.heading}
+              </p>
+              <p className={`text-xs leading-4 font-semibold ${testimonial.quoteStyle}`}>
+                "{testimonial.testimonialParagraph}"
+              </p>
+            </div>
           </div>
         );
       })}
