e6538c9c8ebca1e64985ed850555d9e6b068836d
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-04-27 20:25:42 +0200

    deleted everything in GridContainer.jsx and added icons in data.json

--- time-tracking-dashboard/src/App.jsx
+++ time-tracking-dashboard/src/App.jsx
@@ -1,6 +1,10 @@
+import GridContainer from "./components/GridContainer";
+
 function App () {
   return (
-    <></>
+    <div className="h-screen w-screen flex justify-center items-center bg-[hsl(226,43%,10%)]">
+      <GridContainer />
+    </div>
   );
 }
 
