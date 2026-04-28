01ae959b1274d5dc64f7efca6d51f6c8bed7bee6
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-04-28 21:02:13 +0200

    Added logic and styled the page

--- time-tracking-dashboard/src/App.jsx
+++ time-tracking-dashboard/src/App.jsx
@@ -2,7 +2,7 @@ import GridContainer from "./components/GridContainer";
 
 function App () {
   return (
-    <div className="h-screen flex items-center bg-[hsl(226,43%,10%)] px-16">
+    <div className="font-['Rubik',sans-serif] h-screen flex items-center bg-[hsl(226,43%,10%)] px-32 py-64">
       <GridContainer />
     </div>
   );
--- time-tracking-dashboard/src/components/GridContainer.jsx
+++ time-tracking-dashboard/src/components/GridContainer.jsx
@@ -4,6 +4,7 @@ const timeframes = ["daily", "weekly", "monthly"];
 
 function GridContainer () {
 const [data, setData] = useState(null);
+const [timeframe, setTimeframe] = useState("weekly");
 
 useEffect(() => {
   fetch("/src/data/data.json")
@@ -25,7 +26,7 @@ if(!data) return <div>Loading...</div>
         <div className="row-span-1 flex flex-col justify-center bg-[hsl(235,46%,20%)] rounded-b-md p-4">
           {timeframes.map(t => {
             return (
-              <span key={t}>
+              <span key={t} onClick={() => setTimeframe(t)} className={`${t === timeframe ? "text-[hsl(0,100%,100%)]" : "text-[hsl(236,100%,87%)]"}`}>
                 {t}
               </span>
             );
@@ -34,28 +35,37 @@ if(!data) return <div>Loading...</div>
       </div>
       
       <div className="col-span-3 grid md:grid-cols-3 md:grid-rows-2 gap-4">
-        {data.map((activity) => {
+        {data.map(activity => {
+          const { current, previous } = activity.timeframes[timeframe];
+          const label = timeframe === "daily" ? "Yesterday" : timeframe === "weekly" ? "Last Week" :  "Last Month";
+
           return (
-            <div 
-              key={activity.title}
-              className="bg-[hsl(235,46%,20%)] text-[hsl(0,100%,100%)] flex flex-col gap-4 p-4 rounded-md"
+            <div key={activity.title} className={`md:grid md:grid-rows-5 ${activity.backgroundColor} overflow-hidden rounded-2xl`}>
+              <div className="md:row-span-1 relative">
+                <img className="h-12 absolute right-1" src={activity.icon} alt={`${activity.title} top bar`} />
+              </div>
+              <div 
+              
+              className="bg-[hsl(235,46%,20%)] text-[hsl(0,100%,100%)] flex flex-col gap-2 p-4 rounded-t-2xl md:row-span-4 z-50"
             >
               <div className="flex justify-between items-center">
-                <span className="font-semibold">
+                <span className="font-semibold text-sm">
                   {activity.title}
                 </span>
                 <svg className="fill-white w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M96 320C96 289.1 121.1 264 152 264C182.9 264 208 289.1 208 320C208 350.9 182.9 376 152 376C121.1 376 96 350.9 96 320zM264 320C264 289.1 289.1 264 320 264C350.9 264 376 289.1 376 320C376 350.9 350.9 376 320 376C289.1 376 264 350.9 264 320zM488 264C518.9 264 544 289.1 544 320C544 350.9 518.9 376 488 376C457.1 376 432 350.9 432 320C432 289.1 457.1 264 488 264z"/></svg>
               </div>
 
-              <h1 className="text-5xl">
-                {activity.timeframes.weekly.current}
+              <h1 className="text-4xl">
+                {current}hrs
               </h1>
 
-              <p>
-                Last week- 5hrs
+              <p className="text-sm">
+                {label}- {previous}hrs
               </p>
             </div>
-          )
+            </div>
+            
+          );
         })}
       </div>
     </div>
