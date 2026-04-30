5e424f3c977d68be9cb173f0131660d0e982dac3
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-04-30 23:32:49 +0200

    Added the desserts grid and moved assets and the json file into the public folder

--- product-list-with-cart/src/App.jsx
+++ product-list-with-cart/src/App.jsx
@@ -1,7 +1,9 @@
+import Container from "./components/Container";
+
 function App () {
   return (
-    <div className="font-['Red Hat Text',sans-serif]">
-      
+    <div className="font-['Red Hat Text',sans-serif] bg-[hsl(13,31%,94%)]">
+      <Container />
     </div>
   );
 }
